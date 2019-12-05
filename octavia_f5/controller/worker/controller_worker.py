2b4bebc294029a7ad734d8f56d553cf6a1685b9d
Benjamin Ludwig <benjamin.ludwig@sap.com>
2019-12-05 18:34:52 +0100

    Refactor: Remove unnecessary module controller_worker

--- octavia_f5/controller/worker/controller_worker.py
+++ octavia_f5/controller/worker/controller_worker.py
@@ -15,6 +15,7 @@
 
 import oslo_messaging as messaging
 import tenacity
+import uuid
 from octavia_lib.api.drivers import driver_lib
 from octavia_lib.api.drivers import exceptions as driver_exceptions
 from oslo_config import cfg
@@ -23,8 +24,15 @@ from sqlalchemy.orm import exc as db_exceptions
 
 from octavia.db import repositories as repo
 from octavia_f5.common import constants
-from octavia_f5.controller.worker.f5agent_driver import tenant_update
 from octavia_f5.db import api as db_apis
+from octavia_f5.restclient.as3classes import ADC, AS3, Application
+from octavia_f5.restclient.as3objects import application as m_app
+from octavia_f5.restclient.as3objects import monitor as m_monitor
+from octavia_f5.restclient.as3objects import policy_endpoint as m_policy
+from octavia_f5.restclient.as3objects import pool as m_pool
+from octavia_f5.restclient.as3objects import pool_member as m_member
+from octavia_f5.restclient.as3objects import service as m_service
+from octavia_f5.restclient.as3objects import tenant as m_part
 from octavia_f5.restclient.as3restclient import BigipAS3RestClient
 from octavia_f5.utils import esd_repo
 
@@ -36,6 +44,69 @@ RETRY_ATTEMPTS = 15
 RETRY_INITIAL_DELAY = 1
 RETRY_BACKOFF = 1
 RETRY_MAX = 5
+
+
+def _update_tenant(project_id, loadbalancers, bigip, action='deploy'):
+    """Task to update F5s with all specified loadbalancers' configurations
+       of a tenant (project).
+
+    """
+    decl = AS3(
+        persist=True,
+        action=action)
+    adc = ADC(
+        id="urn:uuid:{}".format(uuid.uuid4()),
+        label="F5 BigIP Octavia Provider")
+    decl.set_adc(adc)
+
+    tenant = adc.get_or_create_tenant(
+        m_part.get_name(project_id))
+
+    for loadbalancer in loadbalancers:
+        # Create generic application
+        app = Application(constants.APPLICATION_GENERIC,
+                          label=loadbalancer.id)
+
+        # attach listeners with ESDs / L7Policies
+        for listener in loadbalancer.listeners:
+            profiles = {}
+            for l7policy in listener.l7policies:
+                esd = bigip.esd.get_esd(l7policy.name)
+                if esd:
+                    profiles.update(m_service.process_esd(esd))
+                else:
+                    app.add_policy_endpoint(
+                        m_policy.get_name(l7policy.id),
+                        m_policy.get_endpoint_policy(l7policy)
+                    )
+            app.add_service(
+                m_service.get_name(listener.id),
+                m_service.get_service(listener)
+            )
+
+        # attach pools
+        for pool in loadbalancer.pools:
+            as3pool = m_pool.get_pool(pool)
+
+            for member in pool.members:
+                as3pool.add_member(
+                    m_member.get_member(member))
+
+            if pool.health_monitor:
+                app.add_monitor(
+                    m_monitor.get_name(pool.health_monitor.id),
+                    m_monitor.get_monitor(pool.health_monitor))
+
+            app.add_pool(
+                m_pool.get_name(pool.id),
+                as3pool)
+
+        tenant.add_application(
+            m_app.get_name(loadbalancer.id),
+            app)
+
+    # send to bigip
+    return bigip.post(json=decl.to_json())
 
 
 class ControllerWorker(object):
@@ -95,7 +166,7 @@ class ControllerWorker(object):
 
     def refresh(self, ctxt, project_id):
         loadbalancers = self._get_all_loadbalancer(project_id)
-        if tenant_update(project_id, loadbalancers, self.bigip, action='dry-run'):
+        if _update_tenant(project_id, loadbalancers, self.bigip, action='dry-run'):
             for lb in loadbalancers:
                 status_active = {"loadbalancers": [{"id": lb.id,
                                                     "provisioning_status": "ACTIVE",
