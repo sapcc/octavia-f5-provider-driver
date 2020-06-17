daef7bc4c3061b371ae76c76959eeadb5ed60a30
Andrew Karpow <andrew.karpow@sap.com>
2020-06-17 13:28:36 +0200

    [as3pool_member] normalize member weight to [0, 100]
    AS3 accepts ratios between 0 and 100 whereas Octavia
    allows ratios in a range of 0 and 256, so we normalize
    the Octavia value for AS3 consumption.

--- octavia_f5/restclient/as3objects/pool_member.py
+++ octavia_f5/restclient/as3objects/pool_member.py
@@ -11,6 +11,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import math
 
 from octavia_f5.common import constants
 from octavia_f5.restclient import as3types
@@ -20,6 +21,17 @@ from octavia_f5.restclient.as3classes import Member
 def get_name(member_id):
     return "{}{}".format(constants.PREFIX_MEMBER, member_id)
 
+
+def normalize_weight(weight):
+    """AS3 accepts ratios between 0 and 100 whereas Octavia
+    allows ratios in a range of 0 and 256, so we normalize
+    the Octavia value for AS3 consumption.
+
+    We also round up the result since we want avoid having a 0
+    (no traffic received at all) when setting a weight of 1
+    """
+    ratio = int(math.ceil((weight / 256.) * 100.))
+    return ratio
 
 def get_member(member):
     args = dict()
@@ -33,9 +45,9 @@ def get_member(member):
 
     if member.weight == 0:
         args['ratio'] = 1
-        args['adminState'] = "disable"
+        args['adminState'] = 'disable'
     else:
-        args['ratio'] = member.weight
+        args['ratio'] = normalize_weight(member.weight)
 
     args['remark'] = as3types.f5remark(member.id)
     return Member(**args)
