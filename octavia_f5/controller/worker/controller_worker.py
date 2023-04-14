sapcc/octavia-f5-provider-driver

commit 101fa5f21b586bafb043a76daab648ad03505cff

Reschedule capability

octavia_f5/controller/worker/controller_worker.py
@@ -23,6 +23,7 @@ import prometheus_client as prometheus
 import tenacity
 from futurist import periodics
 from octavia_lib.common import constants as lib_consts
+from oslo_concurrency import lockutils
 from oslo_config import cfg
 from oslo_db import api as oslo_db_api
 from oslo_log import log as logging
@@ -106,37 +107,40 @@ class ControllerWorker(object):
         super(ControllerWorker, self).__init__()
 
     def as3worker(self):
+        @lockutils.synchronized("f5sync", fair=True)
+        def f5sync(*args):
+            self._metric_as3worker_queue.labels(octavia_host=CONF.host).set(self.queue.qsize())
+            network_id, device = self.queue.get()
+            loadbalancers = self._get_all_loadbalancer(network_id)
+            LOG.debug("AS3Worker after pop (queue_size=%d): Refresh tenant '%s' with loadbalancer %s",
+                      self.queue.qsize(), network_id, [lb.id for lb in loadbalancers])
+            selfips = list(chain.from_iterable(
+                self.network_driver.ensure_selfips(loadbalancers, CONF.host, cleanup_orphans=True)))
+            if all(lb.provisioning_status == lib_consts.PENDING_DELETE for lb in loadbalancers):
+                self.sync.tenant_delete(network_id, device).raise_for_status()
+                # Cleanup l2 configuration and remove selfip ports
+                self.l2sync.remove_l2_flow(network_id, device)
+                self.network_driver.cleanup_selfips(selfips)
+            else:
+                if all(lb.provisioning_status in [lib_consts.PENDING_CREATE, lib_consts.PENDING_DELETE]
+                       for lb in loadbalancers):
+                    # Network is new - ensure complete l2 flow
+                    self.l2sync.ensure_l2_flow(selfips, network_id, device)
+                elif any(lb.provisioning_status in [lib_consts.PENDING_CREATE, lib_consts.PENDING_DELETE]
+                         for lb in loadbalancers):
+                    # Network already exists, just ensure correct selfips
+                    self.l2sync.sync_l2_selfips_flow(selfips, network_id, device)
+                self.sync.tenant_update(network_id, device, selfips).raise_for_status()
+
+            # update status of just-synced LBs
+            self.status.update_status(loadbalancers)
+            for lb in loadbalancers:
+                self._reset_in_use_quota(lb.project_id)
+
         """ AS3 Worker thread, pops tenant to refresh from thread-safe set queue"""
         while True:
             try:
-                self._metric_as3worker_queue.labels(octavia_host=CONF.host).set(self.queue.qsize())
-                network_id, device = self.queue.get()
-                loadbalancers = self._get_all_loadbalancer(network_id)
-                LOG.debug("AS3Worker after pop (queue_size=%d): Refresh tenant '%s' with loadbalancer %s",
-                          self.queue.qsize(), network_id, [lb.id for lb in loadbalancers])
-                selfips = list(chain.from_iterable(
-                    self.network_driver.ensure_selfips(loadbalancers, CONF.host, cleanup_orphans=True)))
-                if all(lb.provisioning_status == lib_consts.PENDING_DELETE for lb in loadbalancers):
-                    self.sync.tenant_delete(network_id, device).raise_for_status()
-                    # Cleanup l2 configuration and remove selfip ports
-                    self.l2sync.remove_l2_flow(network_id, device)
-                    self.network_driver.cleanup_selfips(selfips)
-                else:
-                    if all(lb.provisioning_status in [lib_consts.PENDING_CREATE, lib_consts.PENDING_DELETE]
-                           for lb in loadbalancers):
-                        # Network is new - ensure complete l2 flow
-                        self.l2sync.ensure_l2_flow(selfips, network_id, device)
-                    elif any(lb.provisioning_status in [lib_consts.PENDING_CREATE, lib_consts.PENDING_DELETE]
-                             for lb in loadbalancers):
-                        # Network already exists, just ensure correct selfips
-                        self.l2sync.sync_l2_selfips_flow(selfips, network_id, device)
-                    self.sync.tenant_update(network_id, device, selfips).raise_for_status()
-
-                # update status of just-synced LBs
-                self.status.update_status(loadbalancers)
-                for lb in loadbalancers:
-                    self._reset_in_use_quota(lb.project_id)
-
+                f5sync()
             except Empty:
                 # Queue empty, pass
                 pass
@@ -582,11 +586,67 @@ class ControllerWorker(object):
     def failover_loadbalancer(self, load_balancer_id):
         pass
 
-    def migrate_loadbalancer(self, load_balancer_id, target_host):
-        pass
+    @tenacity.retry(
+        retry=tenacity.retry_if_exception_type(db_exceptions.NoResultFound),
+        wait=tenacity.wait_incrementing(
+            RETRY_INITIAL_DELAY, RETRY_BACKOFF, RETRY_MAX),
+        stop=tenacity.stop_after_attempt(RETRY_ATTEMPTS))
+    @lockutils.synchronized("f5sync", fair=True)
+    def add_loadbalancer(self, load_balancer_id):
+        # forcing a loadbalancer sync even if it's not currently scheduled
+        lb = self._loadbalancer_repo.get(db_apis.get_session(), id=load_balancer_id)
+        if not lb:
+            LOG.warning('Failed to fetch %s %s from DB. Retrying for up to '
+                        '60 seconds.', 'loadbalancer', load_balancer_id)
+            raise db_exceptions.NoResultFound
 
-    def migrate_loadbalancers(self, source_host, target_host):
-        pass
+        network_id = lb.vip.network_id
+        LOG.debug("add_loadbalancer: force adding loadbalancer '%s' for tenant '%s'",
+                  load_balancer_id, network_id)
+
+        loadbalancers = self._get_all_loadbalancer(network_id)
+        if load_balancer_id not in [_lb.id for _lb in loadbalancers]:
+            loadbalancers.append(lb)
+
+        selfips = list(chain.from_iterable(
+            self.network_driver.ensure_selfips(loadbalancers, CONF.host, cleanup_orphans=False)))
+        self.l2sync.ensure_l2_flow(selfips, network_id)
+        self.sync.tenant_update(network_id, selfips=selfips, loadbalancers=loadbalancers).raise_for_status()
+        self.network_driver.invalidate_cache()
+        return True
+
+    @tenacity.retry(
+        retry=tenacity.retry_if_exception_type(db_exceptions.NoResultFound),
+        wait=tenacity.wait_incrementing(
+            RETRY_INITIAL_DELAY, RETRY_BACKOFF, RETRY_MAX),
+        stop=tenacity.stop_after_attempt(RETRY_ATTEMPTS))
+    @lockutils.synchronized("f5sync", fair=True)
+    def remove_loadbalancer(self, load_balancer_id):
+        # forcing a loadbalancer sync even if it's not currently scheduled
+        lb = self._loadbalancer_repo.get(db_apis.get_session(), id=load_balancer_id)
+        if not lb:
+            LOG.warning('Failed to fetch %s %s from DB. Retrying for up to '
+                        '60 seconds.', 'loadbalancer', load_balancer_id)
+            raise db_exceptions.NoResultFound
+
+        network_id = lb.vip.network_id
+        LOG.debug("remove_loadbalancer: force removing loadbalancer '%s' for tenant '%s'",
+                  load_balancer_id, network_id)
+
+        loadbalancers = self._get_all_loadbalancer(network_id)
+        loadbalancers = [_lb for _lb in loadbalancers if _lb.id != load_balancer_id]
+
+        selfips = list(chain.from_iterable(
+            self.network_driver.ensure_selfips(loadbalancers, CONF.host, cleanup_orphans=False)))
+        if loadbalancers:
+            self.l2sync.ensure_l2_flow(selfips, network_id)
+            self.sync.tenant_update(network_id, selfips=selfips, loadbalancers=loadbalancers).raise_for_status()
+        else:
+            self.sync.tenant_delete(network_id).raise_for_status()
+            self.l2sync.remove_l2_flow(network_id)
+            self.network_driver.cleanup_selfips(selfips)
+        self.network_driver.invalidate_cache()
+        return True
 
     def amphora_cert_rotation(self, amphora_id):
         pass

octavia_f5/api/drivers/f5_driver/arbiter.py
@@ -1,0 +1,93 @@
+# Copyright 2022 SAP SE
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import abc
+
+from oslo_config import cfg
+from oslo_log import log as logging
+from taskflow import flow
+from taskflow.listeners import logging as tf_logging
+from taskflow.patterns import linear_flow, unordered_flow
+
+from octavia.common import base_taskflow
+from octavia_f5.controller.worker.tasks import network_tasks
+from octavia_f5.api.drivers.f5_driver.tasks import reschedule_tasks
+from octavia_f5.utils import driver_utils
+
+CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
+
+
+class RescheduleMixin(object, metaclass=abc.ABCMeta):
+    """Abstract mixin class for reschedule support in loadbalancer RPC
+
+    """
+    def loadbalancer_add(self, loadbalancer_id, target_host):
+        """Forces a loadbalancer to be added to the target host
+
+        :param loadbalancer_id: loadbalancer id
+        :param target_host: agent
+        """
+
+    def loadbalancer_remove(self, loadbalancer_id, target_host):
+        """Forces a loadbalancer to be removed from the target host
+
+        :param loadbalancer_id: loadbalancer id
+        :param target_host: agent
+        """
+
+
+class MigrationArbiter(RescheduleMixin):
+    def __init__(self):
+        self.tf_engine = base_taskflow.BaseTaskFlowEngine()
+        self.network_driver = driver_utils.get_network_driver()
+
+    def run_flow(self, func, *args, **kwargs):
+        tf = self.tf_engine.taskflow_load(
+            func(*args), **kwargs)
+        with tf_logging.DynamicLoggingListener(tf, log=LOG):
+            tf.run()
+
+    def get_reschedule_flow(self) -> flow.Flow:
+        # Prepare Self-IPs for target
+        get_loadbalancer_task = reschedule_tasks.GetLoadBalancerByID()
+        create_selfips_task = network_tasks.CreateSelfIPs(self.network_driver)
+        wait_for_selfip_task = network_tasks.WaitForNewSelfIPs(self.network_driver)
+
+        add_loadbalancer_task = reschedule_tasks.ForceAddLoadbalancer(rpc=self)
+        get_old_agent_task = reschedule_tasks.GetOldAgentFromLoadBalancer()
+        remove_loadbalancer_task = reschedule_tasks.ForceDeleteLoadbalancer(rpc=self)
+        rewrite_loadbalancer_task = reschedule_tasks.RewriteLoadBalancerEntry()
+        rewrite_amphora_task = reschedule_tasks.RewriteAmphoraEntry()
+
+        all_selfips_task = network_tasks.AllSelfIPs(self.network_driver)
+        get_vip_port_task = network_tasks.GetVIPFromLoadBalancer(self.network_driver)
+        update_aap_task = network_tasks.UpdateAAP(self.network_driver)
+        update_vip_task = network_tasks.UpdateVIP(self.network_driver)
+        invalidate_cache_task = network_tasks.InvalidateCache(self.network_driver)
+
+        add_remove_loadbalancer_flow = unordered_flow.Flow('add-remove-lb-flow')
+        add_remove_loadbalancer_flow.add(add_loadbalancer_task, remove_loadbalancer_task)
+
+        update_database_flow = unordered_flow.Flow("database-update-flow")
+        update_database_flow.add(rewrite_loadbalancer_task, rewrite_amphora_task, invalidate_cache_task)
+
+        update_vip_flow = linear_flow.Flow("update-vip-flow")
+        update_vip_flow.add(get_vip_port_task, update_vip_task, all_selfips_task, update_aap_task)
+
+        reschedule_flow = linear_flow.Flow('reschedule-flow')
+        reschedule_flow.add(get_loadbalancer_task, get_old_agent_task, create_selfips_task,
+                            wait_for_selfip_task, add_remove_loadbalancer_flow,
+                            update_database_flow, update_vip_flow)
+        return reschedule_flow

octavia_f5/api/drivers/f5_driver/driver.py
@@ -20,6 +20,8 @@ from oslo_log import log as logging
 from octavia.api.drivers.amphora_driver.v1 import driver
 from octavia.common import constants as consts
 from octavia.db import api as db_apis
+
+from octavia_f5.api.drivers.f5_driver import arbiter
 from octavia_f5.utils import driver_utils
 
 CONF = cfg.CONF
@@ -27,7 +29,9 @@ CONF.import_group('oslo_messaging', 'octavia.common.config')
 LOG = logging.getLogger(__name__)
 
 
-class F5ProviderDriver(driver.AmphoraProviderDriver):
+class F5ProviderDriver(driver.AmphoraProviderDriver,
+                       arbiter.MigrationArbiter,
+                       arbiter.RescheduleMixin):
     """Octavia plugin for the F5 driver.
 
     Callbacks need to be overwritten for setting the _server_ attribute
@@ -80,14 +84,18 @@ class F5ProviderDriver(driver.AmphoraProviderDriver):
         client.cast({}, 'failover_load_balancer', **payload)
 
     def loadbalancer_migrate(self, loadbalancer_id, target_host):
-        payload = {consts.LOAD_BALANCER_ID: loadbalancer_id, 'target_host': target_host}
-        client = self.client.prepare(server=self._get_server(loadbalancer_id))
-        client.cast({}, 'migrate_load_balancer', **payload)
+        store = {consts.LOADBALANCER_ID: loadbalancer_id, "candidate": target_host}
+        self.run_flow(self.get_reschedule_flow, store=store)
 
-    def loadbalancers_migrate(self, source_host, target_host):
-        payload = {'source_host': source_host, 'target_host': target_host}
-        client = self.client.prepare(server=source_host) # note that the user can inject arbitrary server names
-        client.cast({}, 'migrate_load_balancers', **payload)
+    def loadbalancer_add(self, loadbalancer_id, target_host):
+        payload = {consts.LOAD_BALANCER_ID: loadbalancer_id}
+        client = self.client.prepare(server=target_host)
+        client.call({}, 'add_loadbalancer', **payload)
+
+    def loadbalancer_remove(self, loadbalancer_id, target_host):
+        payload = {consts.LOAD_BALANCER_ID: loadbalancer_id}
+        client = self.client.prepare(server=target_host)
+        client.call({}, 'remove_loadbalancer', **payload)
 
     def loadbalancer_update(self, old_loadbalancer, new_loadbalancer):
         lb_id = new_loadbalancer.loadbalancer_id

octavia_f5/api/drivers/f5_driver/tasks/reschedule_tasks.py
@@ -1,0 +1,125 @@
+# Copyright 10/6/22 SAP SE
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from abc import ABCMeta
+
+from oslo_config import cfg
+from oslo_log import log as logging
+from taskflow import task
+from taskflow.types import failure
+
+from octavia.common import constants
+from octavia.common import data_models as models
+from octavia.db import api as db_apis
+from octavia_f5.db import repositories as repo
+
+LOG = logging.getLogger(__name__)
+CONF = cfg.CONF
+
+
+class RescheduleTasks(task.Task, metaclass=ABCMeta):
+    """Base task to load drivers common to the tasks."""
+    def __init__(self, **kwargs):
+        self.rpc = kwargs.pop("rpc", None)
+        super(RescheduleTasks, self).__init__(**kwargs)
+        self._loadbalancer_repo = repo.LoadBalancerRepository()
+        self._amphora_repo = repo.AmphoraRepository()
+
+
+class GetLoadBalancerByID(RescheduleTasks):
+    default_provides = 'load_balancer'
+
+    def execute(self, loadbalancer_id):
+        LOG.debug("Get load balancer from DB by id: %s ", loadbalancer_id)
+        return self._loadbalancer_repo.get(db_apis.get_session(),
+                                           id=loadbalancer_id)
+
+
+class GetOldAgentFromLoadBalancer(RescheduleTasks):
+    default_provides = 'removal_host'
+
+    def execute(self, load_balancer: models.LoadBalancer):
+        return load_balancer.server_group_id
+
+
+class ForceAddLoadbalancer(RescheduleTasks):
+    def execute(self, loadbalancer_id: str, candidate: str):
+        self.rpc.loadbalancer_add(loadbalancer_id=loadbalancer_id,
+                                  target_host=candidate)
+
+    def revert(self, result, loadbalancer_id: str, candidate: str, **kwargs):
+        """Handle a failure to force adding a loadbalancer."""
+
+        if isinstance(result, failure.Failure):
+            LOG.error("ForceAddLoadbalancer: Unable to create loadbalancer")
+            return
+        LOG.warning("ForceAddLoadbalancer: Reverting create loadbalancer %s from %s",
+                    loadbalancer_id, candidate)
+        self.rpc.loadbalancer_remove(loadbalancer_id=loadbalancer_id,
+                                     target_host=candidate)
+
+
+class ForceDeleteLoadbalancer(RescheduleTasks):
+    def execute(self, loadbalancer_id: str, removal_host: str):
+        self.rpc.loadbalancer_remove(loadbalancer_id=loadbalancer_id,
+                                     target_host=removal_host)
+
+    def revert(self, result, loadbalancer_id: str, removal_host: str, **kwargs):
+        """Handle a failure to force adding a loadbalancer."""
+
+        if isinstance(result, failure.Failure):
+            LOG.error("ForceDeleteLoadbalancer: Unable to delete loadbalancer")
+            return
+        LOG.warning("ForceDeleteLoadbalancer: Reverting delete loadbalancer %s from %s",
+                    loadbalancer_id, removal_host)
+        self.rpc.loadbalancer_add(loadbalancer_id=loadbalancer_id,
+                                  target_host=removal_host)
+
+
+class RewriteAmphoraEntry(RescheduleTasks):
+    def execute(self, load_balancer: models.LoadBalancer, candidate: str, *args, **kwargs):
+        LOG.debug("RewriteAmphoraEntry %s: Changing host '%s' to '%s'.",
+                  load_balancer.id, load_balancer.server_group_id, candidate)
+        self._amphora_repo.update(db_apis.get_session(), load_balancer.id, compute_flavor=candidate)
+        self._loadbalancer_repo.update(db_apis.get_session(), load_balancer.id, server_group_id=candidate,
+                                       provisioning_status=constants.PENDING_CREATE)
+
+    def revert(self, result, load_balancer: models.LoadBalancer, candidate: str, removal_host: str, **kwargs):
+        """Handle a failure to force adding a loadbalancer."""
+
+        if isinstance(result, failure.Failure):
+            LOG.error("RewriteAmphoraEntry: Unable to update Amphora entry for %s",
+                      load_balancer.id)
+            return
+        LOG.warning("RewriteAmphoraEntry: Reverting host change of amphora %s from '%s' to '%s'",
+                    load_balancer.id, candidate, removal_host)
+        self._amphora_repo.update(db_apis.get_session(), load_balancer.id, compute_flavor=removal_host)
+
+
+class RewriteLoadBalancerEntry(RescheduleTasks):
+    def execute(self, load_balancer: models.LoadBalancer, candidate: str, *args, **kwargs):
+        LOG.debug("RewriteLoadBalancerEntry %s: Changing host '%s' to '%s'.",
+                  load_balancer.id, load_balancer.server_group_id, candidate)
+        self._loadbalancer_repo.update(db_apis.get_session(), load_balancer.id, server_group_id=candidate)
+
+    def revert(self, result, load_balancer: models.LoadBalancer, candidate: str, removal_host: str, **kwargs):
+        """Handle a failure to force adding a loadbalancer."""
+
+        if isinstance(result, failure.Failure):
+            LOG.error("RewriteLoadBalancerEntry: Unable to update loadbalancer %s",
+                      load_balancer.id)
+            return
+        LOG.warning("RewriteLoadBalancerEntry: Reverting host change of loadbalancer %s from '%s' to '%s'",
+                    load_balancer.id, candidate, removal_host)
+        self._loadbalancer_repo.update(db_apis.get_session(), load_balancer.id, server_group_id=removal_host)

octavia_f5/controller/worker/tasks/network_tasks.py
@@ -69,6 +69,30 @@ class AllSelfIPs(BaseNetworkTask):
         return existing_selfips + new_selfips
 
 
+class CreateSelfIP(BaseNetworkTask):
+    default_provides = "port"
+    def execute(self, loadbalancer: dict,
+                candidate: str) -> network_models.Port:
+        return self.network_driver.create_selfip(loadbalancer, f5host="todo", agent=candidate)
+
+    def revert(self, result: network_models.Port,
+               *args, **kwargs):
+        """Handle a failure to create selfip port."""
+
+        if isinstance(result, failure.Failure):
+            LOG.error("CreateSelfIP: Unable to create selfips")
+            return
+        LOG.warning("Reverting: deleting selfip port %s", result.name)
+        self.network_driver.delete_port(result.id)
+
+
+class WaitForNewSelfIPs(BaseNetworkTask):
+    def execute(self, new_selfips: [network_models.Port]):
+        # Wait for port to be active
+        for port in new_selfips:
+            self.network_driver.is_port_active(port.id)
+
+
 class CreateSelfIPs(BaseNetworkTask):
     default_provides = ('existing_selfips', 'new_selfips')
 
@@ -125,10 +149,26 @@ class CreateVIPPort(BaseNetworkTask):
         self.network_driver.delete_port(result.id)
 
 
+class GetVIPFromLoadBalancer(BaseNetworkTask):
+    default_provides = 'vip_port'
+
+    def execute(self, load_balancer: data_models.LoadBalancer) -> network_models.Port:
+        return self.network_driver.get_port(load_balancer.vip.port_id)
+
+
 class UpdateAAP(BaseNetworkTask):
-    def execute(self, vip_port: dict, selfips: [network_models.Port]):
+    def execute(self, vip_port: network_models.Port, selfips: [network_models.Port]):
         # Update allowed address pairs
         self.network_driver.update_aap(vip_port, selfips)
+
+
+class UpdateVIP(BaseNetworkTask):
+    def execute(self, vip_port: network_models.Port, candidate: str):
+        # Update allowed address pairs
+        self.network_driver.update_vip(vip_port, candidate)
+
+    def revert(self, vip_port: network_models.Port, removal_host: str, **kwargs):
+        self.network_driver.update_vip(vip_port, removal_host)
 
 
 class DeleteVIP(BaseNetworkTask):
@@ -166,3 +206,7 @@ class CleanupSelfIPs(BaseNetworkTask):
         if not load_balancers:
             for selfip in selfips:
                 self.network_driver.delete_port(selfip['id'])
+
+class InvalidateCache(BaseNetworkTask):
+    def execute(self, *args, **kwargs):
+        self.network_driver.invalidate_cache()

octavia_f5/network/drivers/neutron/neutron_client.py
@@ -356,6 +356,23 @@ class NeutronClient(neutron_base.BaseNeutronDriver,
              requests.exceptions.ConnectionError)),
         wait=tenacity.wait_incrementing(1, 1, 5),
         stop=tenacity.stop_after_attempt(15))
+    def create_selfip(self, load_balancer: dict, f5host, agent):
+        project_id = load_balancer[lib_consts.PROJECT_ID]
+        network_id = load_balancer[lib_consts.VIP_NETWORK_ID]
+        subnet_id = load_balancer[lib_consts.VIP_SUBNET_ID]
+
+        selfip_dict = self._make_selfip_dict(project_id, network_id, subnet_id, f5host, agent)
+        selfip = self.neutron_client.create_port(selfip_dict).get('port', selfip_dict)
+        return utils.convert_port_dict_to_model(selfip)
+
+    @tenacity.retry(
+        retry=tenacity.retry_if_exception_type(
+            (neutron_client_exceptions.Conflict,
+             neutron_client_exceptions.InternalServerError,
+             neutron_client_exceptions.ServiceUnavailable,
+             requests.exceptions.ConnectionError)),
+        wait=tenacity.wait_incrementing(1, 1, 5),
+        stop=tenacity.stop_after_attempt(15))
     def ensure_selfips(self, load_balancers: [octavia_models.LoadBalancer],
                        agent: str = None,
                        cleanup_orphans: bool = False) -> [network_models.Port]:
@@ -459,6 +476,14 @@ class NeutronClient(neutron_base.BaseNeutronDriver,
             self.neutron_client.update_port(vip.id, aap)
         except Exception as e:
             LOG.warning("Failed updating VIPs allowed_address_pairs %s: %s", vip.id, e)
+
+    def update_vip(self, vip: network_models.Port, candidate: str):
+        host_binding = {
+            'port': {
+                'binding:host_id': candidate
+            }
+        }
+        self.neutron_client.update_port(vip.id, host_binding)
 
     def cleanup_selfips(self, selfips: [network_models.Port]):
         for port in selfips:
@@ -571,3 +596,16 @@ class NeutronClient(neutron_base.BaseNeutronDriver,
 
     def set_port_admin_state_up(self, port_id, state):
         pass
+
+    @tenacity.retry(
+        wait=tenacity.wait_incrementing(1, 5, 60),
+        stop=tenacity.stop_after_attempt(10))
+    def is_port_active(self, port_id):
+        port = self.get_port(port_id)
+        if port.status == "ACTIVE":
+            return True
+
+        raise Exception()
+
+    def invalidate_cache(self, hard=True):
+        cache_region.invalidate(hard=hard)

commit b58a6ff653adfae62ece23ff6071d33b15a7f31c

[rescheduling] Fix: SelfIPs not removed
a1510e4 only switched out ensure_l2_flow for
sync_l2_selfips_and_subnet_routes_flow in the remove_loadbalancer
function, however one issue remained[1]:

When the LB to be removed is the last one on this device, the
shadowing loadbalancers variable is set to an empty array, which is
then supplied as argument to ensure_selfips, which in turn yields no
SelfIP ports. This means that when the else-branch is executed,
selfips will be [], so cleanup_selfips(selfips) won't delete any
ports. This is due to the loadbalancers variable being shadowed. This
error was introduced in 101fa5f with the first rescheduling
implementation.

The fix:
Instead, a new variable loadbalancers_remaining has to be created,
which contains all load balancers on this device except the one that
is to be removed.
- If there are load balancers remaining and the one to be removed was
  the last of its subnet, that subnet's SelfIP ports will be removed
  by ensure_selfips(..., cleanup_orphans=True), before the remaining
  SelfIPs (and subnet routes) are synced.
- If there are no load balancers remaining, then selfips contains only
  the SelfIP ports for the load balancer to be removed, which is then
  cleaned up with everything else L2.

[1] https://github.com/sapcc/octavia-f5-provider-driver/pull/214#pullrequestreview-1383776114

octavia_f5/controller/worker/controller_worker.py
@@ -643,18 +643,31 @@ class ControllerWorker(object):
         LOG.debug("remove_loadbalancer: force removing loadbalancer '%s' for tenant '%s'",
                   load_balancer_id, network_id)
 
+        # all LBs on this device, including the one to be removed
         loadbalancers = self._get_all_loadbalancer(network_id)
-        loadbalancers = [_lb for _lb in loadbalancers if _lb.id != load_balancer_id]
-
         selfips = list(chain.from_iterable(
             self.network_driver.ensure_selfips(loadbalancers, CONF.host, cleanup_orphans=False)))
-        if loadbalancers:
+
+        loadbalancers_remaining = [lb for lb in loadbalancers if lb.id != load_balancer_id]
+        if loadbalancers_remaining:
+            # if there are still load balancers we only need to sync SelfIPs and subnet routes
+
+            # If the subnet of the LB to be removed is now empty, remove its SelfIPs by treating them as orphaned
+            selfips = list(chain.from_iterable(
+                self.network_driver.ensure_selfips(loadbalancers_remaining, CONF.host, cleanup_orphans=True)))
+
+            # provision the rest to the device
             self.l2sync.sync_l2_selfips_and_subnet_routes_flow(selfips, network_id)
-            self.sync.tenant_update(network_id, selfips=selfips, loadbalancers=loadbalancers).raise_for_status()
+            self.sync.tenant_update(
+                network_id, selfips=selfips, loadbalancers=loadbalancers_remaining).raise_for_status()
+
         else:
+            # this was the last load balancer - delete everything
             self.sync.tenant_delete(network_id).raise_for_status()
             self.l2sync.remove_l2_flow(network_id)
             self.network_driver.cleanup_selfips(selfips)
+
+        # invalidate cache so that workers forget about the old host
         self.network_driver.invalidate_cache()
         return True
 