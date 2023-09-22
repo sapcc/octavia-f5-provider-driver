5e1bdf09b2e21b2749103b6f73147fadec5f0244
Vadim Ponomarev <vadim.ponomarev@cloudification.io>
2023-09-22 10:39:53 +0200

    Add mandatory cipher for HTTP2

--- octavia_f5/restclient/as3objects/cipher.py
+++ octavia_f5/restclient/as3objects/cipher.py
@@ -41,7 +41,7 @@ def get_cipher_group_name(object_id, object_type):
     return "{}{}_{}".format(constants.PREFIX_CIPHER_GROUP, object_type.lower(), object_id)
 
 
-def filter_cipher_suites(cipher_suites, object_print_name, object_id):
+def filter_cipher_suites(cipher_suites, object_name, object_id, http2=False):
     """Filter out cipher suites according to blocklist and allowlist.
 
     This is necessary, because there can be invalid cipher suites if e.g. a
@@ -49,8 +49,9 @@ def filter_cipher_suites(cipher_suites, object_print_name, object_id):
     listeners/pools using the cipher suite already existed.
 
     :param cipher_suites: String containing colon-separated list of cipher suites.
-    :param object_print_name: A printable representation of the object to be logged, e.g. "Listener" or "Pool".
+    :param object_name: A printable representation of the object to be logged, e.g. "Listener" or "Pool".
     :param object_id: ID of the object the cipher suites belong to. This is used for logging, so it should be a string.
+    :param http2: HTTP2 protocol using
     :return String containing colon-separated list of non-blocked/allowed cipher suites.
     """
 
@@ -61,18 +62,25 @@ def filter_cipher_suites(cipher_suites, object_print_name, object_id):
     cipher_suites_list = cipher_suites.split(':')
     if rejected_cipher_suites:
         LOG.error("{} object with ID {} has invalid cipher suites which won't be provisioned: {}"
-                  .format(object_print_name, object_id, ', '.join(rejected_cipher_suites)))
+                  .format(object_name, object_id, ', '.join(rejected_cipher_suites)))
         for c in rejected_cipher_suites:
             cipher_suites_list.remove(c)
+
+    # Add required ciphers if HTTP2 is using
+    for cipher in constants.CIPHERS_HTTP2:
+        if cipher not in cipher_suites_list:
+            LOG.warning("mandatory cipher {} was added for {} because HTTP2 is using"
+                        .format(cipher, object_name, object_id))
+            cipher_suites_list.append(cipher)
 
     return cipher_suites_list
 
 
-def get_cipher_rule(ciphers, parent_obj, parent_id):
+def get_cipher_rule(ciphers, parent_obj, parent_id, http2=False):
     rule_name = get_cipher_rule_name(parent_id, parent_obj)
     group_name = get_cipher_group_name(parent_id, parent_obj)
     rule_args = {
-        'cipherSuites': filter_cipher_suites(ciphers, parent_obj, parent_id)
+        'cipherSuites': filter_cipher_suites(ciphers, parent_obj, parent_id, http2)
     }
     group_args = {
         'allowCipherRules': [{'use': rule_name}]
