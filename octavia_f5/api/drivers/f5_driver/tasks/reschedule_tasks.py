a30009b811679efb9b90b7245316adb967ddc95e
Benjamin Ludwig <benjamin.ludwig@sap.com>
2024-10-28 14:57:11 +0100

    Rescheduling: Always set availability zone to target host

--- octavia_f5/api/drivers/f5_driver/tasks/reschedule_tasks.py
+++ octavia_f5/api/drivers/f5_driver/tasks/reschedule_tasks.py
@@ -12,6 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import json
 from abc import ABCMeta
 
 from oslo_config import cfg
@@ -21,7 +22,8 @@ from taskflow.types import failure
 
 from octavia.common import data_models as models
 from octavia.db import api as db_apis
-from octavia_f5.db import repositories as repo
+from octavia.db import repositories as repo
+from octavia_f5.db import repositories as f5_repo
 
 LOG = logging.getLogger(__name__)
 CONF = cfg.CONF
@@ -32,8 +34,9 @@ class RescheduleTasks(task.Task, metaclass=ABCMeta):
     def __init__(self, **kwargs):
         self.rpc = kwargs.pop("rpc", None)
         super(RescheduleTasks, self).__init__(**kwargs)
-        self._loadbalancer_repo = repo.LoadBalancerRepository()
-        self._amphora_repo = repo.AmphoraRepository()
+        self._loadbalancer_repo = f5_repo.LoadBalancerRepository()
+        self._amphora_repo = f5_repo.AmphoraRepository()
+        self._azp_repo = repo.AvailabilityZoneProfileRepository()
 
 
 class GetLoadBalancerByID(RescheduleTasks):
@@ -106,9 +109,22 @@ class RewriteAmphoraEntry(RescheduleTasks):
 
 class RewriteLoadBalancerEntry(RescheduleTasks):
     def execute(self, load_balancer: models.LoadBalancer, candidate: str, *args, **kwargs):
-        LOG.debug("RewriteLoadBalancerEntry %s: Changing host '%s' to '%s'.",
-                  load_balancer.id, load_balancer.server_group_id, candidate)
-        self._loadbalancer_repo.update(db_apis.get_session(), load_balancer.id, server_group_id=candidate)
+        with db_apis.get_session() as session:
+
+            # find out new availability zone
+            azps, _ = self._azp_repo.get_all(session)
+            az = None
+            for azp in azps:
+                azd = json.loads(azp.availability_zone_data)
+                hosts = azd.get('hosts')
+                if hosts and candidate in hosts:
+                    az = azp.name
+                    break
+
+            # adjust load balancer DB entry
+            LOG.debug("RewriteLoadBalancerEntry for LB %s: Changing host from '%s' to '%s' and availability zone from '%s' to '%s'.",
+                      load_balancer.id, load_balancer.server_group_id, candidate, load_balancer.availability_zone, az)
+            self._loadbalancer_repo.update(session, load_balancer.id, server_group_id=candidate, availability_zone=az)
 
     def revert(self, result, load_balancer: models.LoadBalancer, candidate: str, removal_host: str, **kwargs):
         """Handle a failure to force adding a loadbalancer."""
